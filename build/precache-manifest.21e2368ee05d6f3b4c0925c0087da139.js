e84d48d51eec782c0e0ba4ae576e4f463eb0e6d7
1991Pawel <43860287+1991Pawel@users.noreply.github.com>
2020-04-01 11:22:48 +0200

    Add files via upload

---
+++ build/precache-manifest.21e2368ee05d6f3b4c0925c0087da139.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b373b16f1b70e05ca7779dc93dcee5a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "024e9a4aff6a44a56248",
+    "url": "/static/css/main.c5ee27d0.chunk.css"
+  },
+  {
+    "revision": "a1c274e1904e84872338",
+    "url": "/static/js/2.536cfeef.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.536cfeef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024e9a4aff6a44a56248",
+    "url": "/static/js/main.246b7cad.chunk.js"
+  },
+  {
+    "revision": "0d271c0f593b2170c517",
+    "url": "/static/js/runtime-main.ae58d55b.js"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/static/media/bin.650cdc57.svg"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "c2a98d52c32d3ac54000a28d26a38323",
+    "url": "/static/media/tick.c2a98d52.svg"
+  }
+]);
